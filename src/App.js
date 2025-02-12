f325d760ae2c3709fea083708bb688b89f93b191
iamnabil <23001886@siswa.um.edu.my>
2025-02-12 15:17:33 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Personal Financer</h1>
+      </header>
+      <div className="nav">
+     
+          <ul id = "ul-top-hypers">
+            <li>home</li>
+            <li>account</li>
+            <li>transaction</li>
+            <li>spending</li>
+          </ul>
+        
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
