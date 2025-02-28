0a4c9031bb2895fc7083c86cc4b11176024bdca6
iamnabil <23001886@siswa.um.edu.my>
2025-03-01 00:01:14 +0800

    list of account

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,10 +7,29 @@ import {totalSum } from './BudgetChart'
 
 function home() {
   return (
+    <div className='home'>
     <div>
     <h3>Budget Overview</h3>
     <BudgetChart/>
     <div className= "sum">Total : {totalSum}</div>
+    </div>
+    <div>
+      <h3>List of account</h3>
+      <div className="account">
+        <div className="account-item">
+          <h4>Bank Islam</h4>
+          <p>RM 5000</p>
+        </div>
+        <div className="account-item">
+          <h4>Asb</h4>
+          <p>RM 7000</p>
+        </div>
+        <div className="account-item">
+          <h4>Cash</h4>
+          <p>RM 100</p>
+        </div>
+        </div>
+    </div>
     </div>
   )
 }
